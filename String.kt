46010ce15956310e1a9c3e85a6259fa0d248b968
Joyjit Bhandari <54208189+joyjitbhandari@users.noreply.github.com>
2022-06-27 16:59:58 +0530

    Add files via upload

---
+++ String.kt
@@ -0,0 +1,25 @@
+fun main(){
+    val s = "Welcome To Internshala"// here declared a string
+
+    val str = "Welcome to Internshala"
+
+    println(s)
+
+    println(str)
+
+    val stringLength = s.length // here finding the string length
+    println(stringLength)// here printing the string length
+
+    val stringIndex = s.get(12)// here finding particular string index
+    println("The character at index is:")
+    println(stringIndex)
+
+    //String templates
+    val i =10
+    println("the i value is $i")//string templates define by ($) symbol
+
+    //the subSequence() is use for print given range index means print substring.
+    println(s.subSequence(0,7))
+    
+
+}
